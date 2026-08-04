617e842406c6ea7d3f930254dd9d0f8cf30edb16
hoseimaster <hosei.imas@gmail.com>
2026-08-05 05:38:42 +0900

    Create preview-descriptions.js

---
+++ preview-descriptions.js
@@ -0,0 +1,812 @@
+/**
+ * プレビューモーダル専用説明文
+ * previewDescription が空欄なら description が表示されます。
+ */
+
+export const PREVIEW_DESCRIPTIONS = [
+
+    {
+        id: "publication-001",
+        title: "アイマス聖地巡礼集 星井町特集",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-002",
+        title: "アイマス聖地巡礼集2",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-003",
+        title: "小笠原営業記",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-004",
+        title: "\"大学公認サークル\"アイマス研究会結成秘話",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-005",
+        title: "アイマス聖地巡礼集総集編",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-006",
+        title: "アイモバ in 小笠原エリア",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-007",
+        title: "インフィニー 学園祭特別号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-008",
+        title: "アイマス2聖地巡礼集",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-009",
+        title: "インフィニー 2011年冬号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-010",
+        title: "アニマス聖地巡礼集",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-011",
+        title: "アニマス聖地巡礼集2",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-012",
+        title: "港の見える丘公園 携帯クリーナーストラップ",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-013",
+        title: "For my family",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-014",
+        title: "インフィニー 2012年春号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-015",
+        title: "アイマス1・2聖地巡礼集",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-016",
+        title: "インフィニー 7thライブ特別号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-017",
+        title: "波",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-018",
+        title: "アニマス聖地巡礼集R",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-019",
+        title: "星井町 携帯クリーナー",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-020",
+        title: "インフィニー コミックマーケット特別号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-021",
+        title: "インフィニー 学園祭号2012",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-022",
+        title: "アイドルマスター検定",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-023",
+        title: "インフィニー 冬ライブ号2013",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-024",
+        title: "HOSEIM@STER 2013春号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-025",
+        title: "アイドルマスター検定2",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-026",
+        title: "HOSEIM@STER 特別夏号！",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-927",
+        title: "HOSEIM@STER 2013夏号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-027",
+        title: "HOSEIM@STER 2013学園祭号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-028",
+        title: "アニメ「THE IDOLM@STER」コメンタリーCD",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-029",
+        title: "HOSEIM@STER 2014 冬ライブ号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-030",
+        title: "HOSEIM@STER 9th特別号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-031",
+        title: "シンデレラガールズ検定",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-032",
+        title: "ミリオンライブ！検定",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-033",
+        title: "HOSEIM@STER 2014 9th東京号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-034",
+        title: "HOSEIM@STER 2014 学園祭号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-035",
+        title: "CALL GUIDE for CINDERELLA 2nd LIVE",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-036",
+        title: "HOSEIM@STER Million 2nd Call Guide",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-037",
+        title: "シンデレラガールズ検定2",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-038",
+        title: "HOSEIM@STER 10th Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-039",
+        title: "アイマスは人生ゲーム（仮）",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-040",
+        title: "HOSEIM@STER 2015学園祭号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-041",
+        title: "HOSEIM@STER 2015 SideM 1st号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-042",
+        title: "アイドルマスター検定3 10周年記念版",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-043",
+        title: "HOSEIM@STER MILLION 3rd 準備号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-044",
+        title: "THE IDOLM@STER MILLION LIVE 3rdLIVE TOUR CARRABAN BOOK",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-045",
+        title: "HOSEIM@STER CINDERELLA GIRLS 4th LIVE Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-046",
+        title: "HOSEIM@STER 2016 学園祭号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-047",
+        title: "アイドルマスターセンター試験 実践問題集2016",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-048",
+        title: "HOSEIM@STER ライブ会場考察号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-049",
+        title: "HOSEiM@STER PRODUCER MEETING 2017 特別号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-050",
+        title: "HOSEIM@STER 2017 SideM 2nd Shining Side",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-051",
+        title: "HOSEIM@STER 2017 SideM 2nd Brilliant Side",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-052",
+        title: "HOSEIM@STER MILLION 4th MEMORIAL LEAFLET",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-053",
+        title: "HOSEIM@STER CINDERELLA GIRLS 5thLIVE Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-054",
+        title: "HOSEiMM@STER 夏コミ号　作中の料理を作ってみた?",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-055",
+        title: "HOSEiM@STER 2017 学園祭号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-056",
+        title: "HOSEiM@STER 2017 winter 作ってみた? Vol.2",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-057",
+        title: "DRAFT 2017",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-058",
+        title: "HOSEiM@STER FOR PRODUCER MEETING 2018",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-059",
+        title: "HOSEiM@STER 2018 学園祭号",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-060",
+        title: "HOSEi M@STER 2018 Cinderella 6th Leaflet -MetLife Dome-",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-061",
+        title: "HOSEi M@STER 2018 Cinderella 6th Leaflet -NAGOYA DOME-",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-062",
+        title: "SideM検定",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-063",
+        title: "シャイニーカラーズ検定",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-064",
+        title: "ミリシタ検定",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-065",
+        title: "HOSEIM@STER CINDERELLA 7th Leaflet MAKUHARI MESSE",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-066",
+        title: "HOSEIM@STER CINDERELLA 7th Leaflet KYOCERADOME",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-067",
+        title: "Hosei m@ster Cook p@d!! ~作ってみた vol.3~",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-068",
+        title: "HOSEIM@STER SHINYCOLORS 3RD LIVE LEAFLET NAGOYA",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-069",
+        title: "THE IDOLM@STER MILLION LIVE! 7thLIVE Q@MP FLYER!!! Reburn Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-070",
+        title: "HOSEIM@STER SHINYCOLORS 3rd LIVE Leaflet FUKUOKA",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-071",
+        title: "HOSEIM@STER 2021",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-072",
+        title: "THE IDOLM@STER CINDERELLA GIRLS 10th ANNIVERSARY M@GICAL WONDERLAND!!! Merry Maerchen Land Memorial leaflet 2021 10.2-3 @West Japan General Exhibition Center",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-073",
+        title: "シャイニーカラーズ検定Ⅱ",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-074",
+        title: "THE IDOLM@STER SideM 6thLIVE TOUR ～NEXT DESTIN@TION!～ Side TOKYO Memorial leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-075",
+        title: "THE IDOLM@STER CINDERELLA GIRLS 10th ANNIVERSARY M@GICAL WONDERLAND TOUR!!! Tropical Land Memorial leaflet 2022 1.29-30",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-076",
+        title: "THE IDOLM@STER MILLION LIVE! 8thLIVE Twelw@ve Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-077",
+        title: "THE IDOLM@STER CINDERELLA GIRLS 10th ANNIVERSARY M@GICAL WONDERLAND!!!　@BELLUNA DOME 2022.4/2~4/3 MEMORIAL LEAFLET",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-078",
+        title: "THE IDOLM@STER SHINY COLORS 4thLIVE 空は澄み、今を越えて。 MEMORIAL LEAFLET?",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-079",
+        title: "THE IDOLM@STER 765PRO ALLSTARS LIVE SUNRICH COLORFUL MEMORIAL LEAFLET?",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-080",
+        title: "HOSEIM@STER 2022",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-081",
+        title: "THE IDOLM@STER CINDERELLA GIRLS LIKE4LIVE #cg_ootd MEMORIAL LEAFLET",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-082",
+        title: "THE IDOLM@STER SHINY COLORS 283PRODUCTION UNIT LIVE MUGEN BEAT MEMORIAL LEAFLET",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-083",
+        title: "ミリシタ検定Ⅱ",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-084",
+        title: "202301.14-01.15 @Nippon Budokan THE IDOL M@STER MILLION LIVE! 9th LIVE ChoruSp@rklle!!!!! Memorial leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-085",
+        title: "THE IDOLM@STER M@STERS OF IDOL WORLD!!!!! 2023 Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-086",
+        title: "THE IDOLM@STER SHINY COLORS 5thLIVE If I_wings.Memorial Reaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-087",
+        title: "THE IDOLM@STER MILION LIVE! 10thLIVE TOUR Act-1 H@PPY 4 YOU! Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-088",
+        title: "THE IDOLM@STER MILLION LIVE! 10thLIVE 5 TO SP@RKLE Act-2 Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-089",
+        title: "THE IDOLM@STER SideM 8th STAGE ～ALL H@NDS TOGETHER～ MEMORIAL LEAFLET?",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-090",
+        title: "HOSEI M@STER 2023",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-091",
+        title: "THE IDOLM@STER MILLION LIVE! 10thLIVE TOUR Act-4 MILLION THE@TER!!!! Memorial Pamphlet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-092",
+        title: "THE IDOLM@STER SHINY COLORS 6thLIVE TOUR Come and Unite! Fantastic Fireworks Memorial Leaflet?",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-093",
+        title: "HOSEI M@STER 2024",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-094",
+        title: "学マス検定",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-095",
+        title: "学園アイドルマスター The 1st Period　Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-096",
+        title: "THE IDOLM@STER SideM 10th ANNIVERSARY ST@GE ～P@SSION-ING!!!～ Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-097",
+        title: "THE IDOLM@STER 765PRO ALLSTARS LIVE ～NEVER END IDOL!!!!!!!!!!!!!～ Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-098",
+        title: "THE IDOLM@STER M@STERS OF IDOL WORLD 2025 Memorial Leaflet",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-99",
+        title: "ミニキャラシールセット",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-100",
+        title: "限定ポストカード",
+        previewDescription: `
+`
+    },
+
+    {
+        id: "publication-101",
+        title: "HOSEIM@STER 2026",
+        previewDescription: `
+■本誌紹介
+　当会の今ある全てを結集した同人誌になります。
+　掲載されている48枚のイラスト全部新規描き下ろしとなっております。
+　本文30ページ・フルカラー仕様
+
+ ●収録内容
+　【#One Snap Memories】   
+　　総勢26人のアイドルが法政大学周辺スポットへ
+　【概念コーデ特集】
+　　概念コーデまとめ
+　【アイマス プチ統計大作戦！】
+　　・桃井あずきの「遍歴調査」
+　　・八神マキノの「物価調査」 
+　【初心者必見企画】
+　　・アケマス入門
+　　・担当アイドル診断 
+　【おまけ】
+　　・活動記録 2025-2026
+　　・  4コマまんが
+
+●頒布イベント
+　✦ コミックマーケット108
+　　　→ イベント特設ページはこちらから 
+　　　→ コミケWebカタログはこちらから
+　✦ 自主法政祭(予定)
+　　　→ 2026自主法政祭特設ページはこちらから  
+
+ 
+■制作者表記
+●文責
+　　さかうち，カトナ
+●編集
+　　なべ
+●イラスト
+　【表　紙】睡眠
+　【裏表紙】25GR
+　【その他】中田、さかうち、睡眠、25GR、うに、馳倅、ナスナ
+●制作
+　　サノマル、はるともP、KMADH、蟹、すのーら
+●ご協力
+　　・当会会員
+　　・当会OBOGの皆様
+　　・アイマスPの皆様 
+　　・法政大学後援会補助事業
+`
+    },
+
+    {
+        id: "publication-102",
+        title: "",
+        previewDescription: `
+`
+    },
+
+];
+
+/**
+ * 制作物IDからプレビュー専用説明文を取得します。
+ *
+ * 空欄の場合は空文字を返し、
+ * modal-preview.js 側で description にフォールバックします。
+ *
+ * @param {*} publicationId
+ * @returns {string}
+ */
+export function getPreviewDescription(
+    publicationId
+) {
+    const normalizedId =
+        String(
+            publicationId ?? ""
+        ).trim();
+
+    if (!normalizedId) {
+        return "";
+    }
+
+    const item =
+        PREVIEW_DESCRIPTIONS.find(
+            (entry) => {
+                return (
+                    String(
+                        entry.id ?? ""
+                    ).trim() ===
+                    normalizedId
+                );
+            }
+        );
+
+    return String(
+        item?.previewDescription ?? ""
+    ).trim();
+}
+
